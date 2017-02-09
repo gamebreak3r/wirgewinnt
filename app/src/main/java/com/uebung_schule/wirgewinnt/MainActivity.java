22d304b1505fb04e26dd7008d2c1d6faeccee240
zeindlmm <zeindlmeierm@gmx.de>
2017-02-09 09:58:35 +0100

    Mario: Stats

--- app/src/main/java/com/uebung_schule/wirgewinnt/MainActivity.java
+++ app/src/main/java/com/uebung_schule/wirgewinnt/MainActivity.java
@@ -55,6 +55,11 @@ public class MainActivity extends AppCompatActivity{
         switch (item.getItemId()) {
             case R.id.menu_stats:
                 setContentView(R.layout.activity_stats);
+                TextView tv = (TextView) findViewById(R.id.text_stats);
+                tv.setText(PHPConnect.username.toUpperCase() + " Stats" + ":");
+                TextView tvWin = (TextView) findViewById(R.id.text_stats);
+                TextView tvLoses = (TextView) findViewById(R.id.text_stats);
+                
                 return true;
             case R.id.menu_logout:
                 setContentView(R.layout.activity_login);
@@ -236,7 +241,7 @@ public class MainActivity extends AppCompatActivity{
                 TextView passwort = (TextView) findViewById(R.id.loginPasswort);
                 //Wenn die Datenbank geht
                 try {
-                    if (PhpConnect.getLoginTrue(username.getText().toString().trim(), passwort.getText().toString().trim())) {
+                    if (PHPConnect.getLoginTrue(username.getText().toString().trim(), passwort.getText().toString().trim())) {
                         createGeame(v);
                         mainMenu.findItem(R.id.menu_stats).setVisible(true);
                         mainMenu.findItem(R.id.menu_logout).setVisible(true);
@@ -278,7 +283,7 @@ public class MainActivity extends AppCompatActivity{
                     else {
                         if (register_passwort.getText().toString().equals(register_passwort2.getText().toString())) {
                             try {
-                                if (PhpConnect.createNewUser(register_username.getText().toString().trim(), register_passwort.getText().toString())) {
+                                if (PHPConnect.createNewUser(register_username.getText().toString().trim(), register_passwort.getText().toString())) {
                                     Toast.makeText(this, "Der User wurde angelegt", Toast.LENGTH_LONG).show();
                                 } else {
                                     Toast.makeText(this, "Username bereits vergeben!", Toast.LENGTH_LONG).show();
